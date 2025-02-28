ea60abb4f951ec1dcbd0c625650d0ea73feb0a9f
Ryan Schneider <ryanleeschneider@gmail.com>
2025-02-28 14:12:06 -0800

    Fix clippy warnings - use find() instead of filter().next()

--- src/items/hastener.rs
+++ src/items/hastener.rs
@@ -36,9 +36,7 @@ pub fn hastener_used(
     let target_item = owner_items
         .slots
         .iter()
-        .filter_map(|slot| slot.as_ref().copied())
-        .filter(|item| *item != hastener_entity) // Don't target self
-        .next();
+        .filter_map(|slot| slot.as_ref().copied()).find(|item| *item != hastener_entity);
 
     // If we found a target item, trigger the haste event
     if let Some(target_item) = target_item {
